2154d15d84909b78e7a4266591b0e36a1da5bccb
ProjectBackupRahul <projectbackuprahul@gmail.com>
2019-09-10 11:54:45 +0530

    Modified message detail

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 
-const express = require('express'),
-http = require('http'),
-app = express(),
-server = http.createServer(app),
-io = require('socket.io').listen(server);
+  const express = require('express'),
+  http = require('http'),
+  app = express(),
+  server = http.createServer(app),
+  io = require('socket.io').listen(server);
 
     //------------------------------ Web app client ------------------------------//
     const path = require('path')
@@ -11,28 +11,31 @@ io = require('socket.io').listen(server);
     //const io = socketio(server)
     const publicDirectoryPath = path.join(__dirname, './public')
     app.use(express.static(publicDirectoryPath))
+    var getDevice = "";
      
   //------------------------------ Web app client ------------------------------//
 
   // ----------------------------Android App client -------------------------------//
-app.get('/', (req, res) => {
-res.send('Chat Server is running on port 9099')
-});
-io.on('connection', (socket) => {
-console.log('user connected')
+  app.get('/', (req, res) => {
+  res.send('Chat Server is running on port 9099')
+  });
+
+   io.on('connection', (socket) => {
+   console.log('user connected')
         socket.on('join', function(DeviceName) {
         console.log(DeviceName +" : has joined  "  );
         socket.broadcast.emit('Device ',DeviceName +" : has Connected ");
     });
 
-       socket.on('messagedetection', (device,messageContent) => {
+       socket.on('messagedetection', (device,DeviceDetail) => {
        //log the message in console 
-       console.log(device+" :" +messageContent)
+       console.log(device+" :" +DeviceDetail)
+        getDevice = DeviceDetail ;
         //create a message object 
-       let  message = {"message":messageContent, "Device Name":device}
+       let  message = {"message":DeviceDetail, "Device Name":device}
       // send the message to the client side  
        io.emit('message', message );
-      });
+       });
       
     socket.on('disconnect', function() {
     console.log( ' user has left ')
@@ -43,14 +46,12 @@ console.log('user connected')
        //-------------------------------------Android App client------------------------------------//
 
          // ----------------------------------- Web App Client ---------------------------------//
-io.on('connection', socket => {
+   io.on('connection', socket => {
     socket.broadcast.emit("showMessage", { name: 'Anonymous', message: 'A NEW USER HAS JOINED' })
-    socket.on('sendMessage', message => io.emit('showMessage', message, (console.log(message))) 
+    socket.on('sendMessage', message => io.emit('showMessage', getDevice, (console.log(getDevice))) 
     
     )
-
-    
-})
+  })
 
     // ---------------------------------------Web App Client ---------------------------------//
 
